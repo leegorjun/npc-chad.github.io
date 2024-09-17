6f061a1f2b41e6fda0e0704981099fb61ac53eab
ChadLee <2471244751@qq.com>
2024-09-17 21:53:39 +0800

    Site updated: 2024-09-17 21:53:38

---
+++ js/cat.js
@@ -0,0 +1,146 @@
+if (document.body.clientWidth > 992) {
+    function getBasicInfo() {
+        /* 窗口高度 */
+        var ViewH = $(window).height();
+        /* document高度 */
+        var DocH = $("body")[0].scrollHeight;
+        /* 滾動的高度 */
+        var ScrollTop = $(window).scrollTop();
+        /* 可滾動的高度 */
+        var S_V = DocH - ViewH;
+        var Band_H = ScrollTop / (DocH - ViewH) * 100;
+        return {
+            ViewH: ViewH,
+            DocH: DocH,
+            ScrollTop: ScrollTop,
+            Band_H: Band_H,
+            S_V: S_V
+        }
+    };
+    function show(basicInfo) {
+        if (basicInfo.ScrollTop > 0.001) {
+            $(".neko").css('display', 'block');
+        } else {
+            $(".neko").css('display', 'none');
+        }
+    }
+    (function ($) {
+        $.fn.nekoScroll = function (option) {
+            var defaultSetting = {
+                top: '0',
+                scroWidth: 6 + 'px',
+                z_index: 9999,
+                zoom: 0.9,
+                borderRadius: 5 + 'px',
+                right: 60 + 'px',
+                nekoImg: 'img/miaomiao.png',
+                hoverMsg: "喵喵喵~",
+                color: "#6f42c1",
+                during: 500,
+                blog_body: "body",
+            };
+            var setting = $.extend(defaultSetting, option);
+            var getThis = this.prop("className") !== "" ? "." + this.prop("className") : this.prop("id") !== "" ? "#" +
+                this.prop("id") : this.prop("nodeName");
+            if ($(".neko").length == 0) {
+                this.after("<div class=\"neko\" id=" + setting.nekoname + " data-msg=\"" + setting.hoverMsg + "\"></div>");
+            }
+            let basicInfo = getBasicInfo();
+            $(getThis)
+                .css({
+                    'position': 'fixed',
+                    'width': setting.scroWidth,
+                    'top': setting.top,
+                    'height': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 + 'px',
+                    'z-index': setting.z_index,
+                    'background-color': setting.bgcolor,
+                    "border-radius": setting.borderRadius,
+                    'right': setting.right,
+                    'background-image': 'url(' + setting.scImg + ')',
+                    'background-image': '-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)', 'border-radius': '2em',
+                    'background-size': 'contain'
+                });
+            $("#" + setting.nekoname)
+                .css({
+                    'position': 'fixed',
+                    'top': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 - 50 + 'px',
+                    'z-index': setting.z_index * 10,
+                    'right': setting.right,
+                    'background-image': 'url(' + setting.nekoImg + ')',
+                });
+            show(getBasicInfo());
+            $(window)
+                .scroll(function () {
+                    let basicInfo = getBasicInfo();
+                    show(basicInfo);
+                    $(getThis)
+                        .css({
+                            'position': 'fixed',
+                            'width': setting.scroWidth,
+                            'top': setting.top,
+                            'height': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 + 'px',
+                            'z-index': setting.z_index,
+                            'background-color': setting.bgcolor,
+                            "border-radius": setting.borderRadius,
+                            'right': setting.right,
+                            'background-image': 'url(' + setting.scImg + ')',
+                            'background-image': '-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)', 'border-radius': '2em',
+                            'background-size': 'contain'
+                        });
+                    $("#" + setting.nekoname)
+                        .css({
+                            'position': 'fixed',
+                            'top': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 - 50 + 'px',
+                            'z-index': setting.z_index * 10,
+                            'right': setting.right,
+                            'background-image': 'url(' + setting.nekoImg + ')',
+                        });
+                    if (basicInfo.ScrollTop == basicInfo.S_V) {
+                        $("#" + setting.nekoname)
+                            .addClass("showMsg")
+                    } else {
+                        $("#" + setting.nekoname)
+                            .removeClass("showMsg");
+                        $("#" + setting.nekoname)
+                            .attr("data-msg", setting.hoverMsg);
+                    }
+                });
+            this.click(function (e) {
+                btf.scrollToDest(0, 500)
+            });
+            $("#" + setting.nekoname)
+                .click(function () {
+                    btf.scrollToDest(0, 500)
+                });
+            return this;
+        }
+    })(jQuery);
+
+    $(document).ready(function () {
+        //部分自定義
+        $("#myscoll").nekoScroll({
+            bgcolor: 'rgb(0 0 0 / .5)', //背景顏色，沒有繩子背景圖片時有效
+            borderRadius: '2em',
+            zoom: 0.9
+        }
+        );
+        //自定義（去掉以下注釋，並注釋掉其他的查看效果）
+        /*
+        $("#myscoll").nekoScroll({
+//          nekoname:'neko1', //nekoname，相當於id
+//          nekoImg:'img/貓咪.png', //neko的背景圖片
+//          scImg:"img/繩1.png", //繩子的背景圖片
+//          bgcolor:'#1e90ff', //背景顏色，沒有繩子背景圖片時有效
+//          zoom:0.9, //繩子長度的縮放值
+//          hoverMsg:'你好~喵', //鼠標浮動到neko上方的對話框信息
+//          right:'100px', //距離頁面右邊的距離
+//          fontFamily:'楷體', //對話框字體
+//          fontSize:'14px', //對話框字體的大小
+//          color:'#1e90ff', //對話框字體顏色
+//          scroWidth:'8px', //繩子的寬度
+//          z_index:100, //不用解釋了吧
+//          during:1200, //從頂部到底部滑動的時長
+        });
+        */
+    })
+}
